cbbf1fb324140612951f0e4011b88c566e85842b
Zainab Salih <Zainab.Salih@outlook.com>
2024-03-23 00:21:06 +0000

    TASK 1{MODIFIED}

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
-# flask from pip3
+# Installs flask, version 2.1.0.
 
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  unless   => '/usr/bin/pip3 show flask | grep -q "^Version: 2.1.0$"',
 }
