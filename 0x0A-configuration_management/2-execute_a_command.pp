355d495f66b2b677fc4f212df3fae3b72a6469ee
Zainab Salih <Zainab.Salih@outlook.com>
2024-03-23 00:00:28 +0000

    TASK 1 & 2{MODIFIED}

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,6 @@
 # create a manifest that kills a process named killmenow.
 
-exec { 'kill_killmenow_process':
-  command     => '/usr/bin/pkill killmenow',
-  refreshonly => true,
-  onlyif      => '/usr/bin/pgrep killmenow',
+exec { 'pkill killmenow' :
+   path    => '/bin/',
+   command => 'pkill killmenow',
 }
