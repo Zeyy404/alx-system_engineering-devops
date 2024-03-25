16a5d3af65afe4cb0bc51578a57b911792c9ccef
Zainab Salih <Zainab.Salih@outlook.com>
2024-03-25 00:25:20 +0000

    ssh_config using puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+# using Puppet to make changes to our configuration file.
+# so that you can connect to a server without typing a password.
+
+include stdlib
+
+file_line { 'SSH Private Key':
+  ensure  => present,
+  path	  => 'etc/ssh/ssh_config',
+  line	  => 'IdentifyFile ~/.ssh/school'
+}
+
+file_line { 'Deny Password Auth':
+  ensure  => present,
+  path	  => 'etc/ssh/ssh_config',
+  line	  => PasswordAuthentication no'
+}
