5281389ab60aeb710d2ba853494c601d2449ddfb
Zainab Salih <Zainab.Salih@outlook.com>
2024-05-20 23:42:03 +0000

    Task 0 - gathering data{Modified} & Task 1 - export to csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """A script that, using REST API, for a given employee ID,
    returns information about his/her TODO list progress."""
+import csv
 import requests
 import sys
 
@@ -20,7 +21,15 @@ if __name__ == "__main__":
     user = requests.get(url + "users/{}".format(employee_id)).json()
     todos = requests.get(url + "todos", params={"userId": employee_id}).json()
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+    username = user.get("username")
+    csv_data = [
+        [employee_id, username, t.get("completed"), t.get("title")]
+        for t in todos
+    ]
+
+    file_name = "{}.csv".format(employee_id)
+    with open(file_name, mode='w', newline='') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        writer.writerow(["USER_ID", "USERNAME",
+                         "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        writer.writerows(csv_data)
