d1cbb930b0241529d6e5d786de3715492fe81fbc
Zainab Salih <Zainab.Salih@outlook.com>
2024-06-18 19:28:13 +0000

    Task 0 - subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,7 +12,7 @@ def number_of_subscribers(subreddit):
     A function that queries the Reddit API for a given subreddit
     Return: 0 if invalid subreddit is given
     """
-    headers = {'User-Agent': 'Custom'}
+    headers = {"User-Agent": "Custom"}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
     try:
