76360d5235bcb333ffe55473699ad362f52ed80b
Zainab Salih <Zainab.Salih@outlook.com>
2024-06-18 19:24:59 +0000

    Task 0 - subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -19,8 +19,7 @@ def number_of_subscribers(subreddit):
         response = requests.get(url, headers=headers, allow_redirects=False)
 
         if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
+            return response.json().get("data").get("subscribers")
         else:
             return 0
     except requests.exceptions.RequestException:
